07863276c0e12a1beb59ba0d56e2172ca7f14a84
amartushy <55608214+amartushy@users.noreply.github.com>
2021-08-05 17:51:57 -0700

    Update and rename HelperFunctions0-0-2.js to HelperFunctions0-0-3.js

--- HelperFunctions0-0-3.js
+++ HelperFunctions0-0-3.js
@@ -29,3 +29,19 @@ function getFirstName(name) {
     let nameArray = name.split(' ')
     return(nameArray[0])
 }
+
+function getCurrentMonthAndYear() {
+    var currentDate = new Date()
+    var currentMonth = currentDate.getMonth()
+    var currentYear = currentDate.getFullYear()
+    return [currentYear, currentMonth]
+}
+
+function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate()
+}
+
+function firstDayOfMonth(year, month) {
+    var firstDate = new Date(year, month, 1)
+    return firstDate.getDay()
+}
